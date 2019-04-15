b7f8fb33f1d28d9e06e8f90f6403d0c6264c4de9
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-04-15 20:29:44 +0000

    Implement pagination

--- table.js
+++ table.js
@@ -1,9 +1,12 @@
-const baseURL = "https://swapi.co/api/";
+/*-----For the pagination to work, we need to pass in our actual URL into the button, instead of a baseURL
+To do this, we need get rid of our baseURL const and to change the 'values' in the button HTML code to be the actual URL
+Then we need to change the type and baseURL arguments in the functions to url*/
+//const baseURL = "https://swapi.co/api/";
 
-function getData(type, cb) {
+function getData(url, cb) {
     var xhr = new XMLHttpRequest();
     
-    xhr.open("GET", baseURL + type + "/");
+    xhr.open("GET", url);
     xhr.send();
 
     xhr.onreadystatechange = function() {
@@ -28,14 +31,39 @@ function getTableHeaders(obj) {
     return `<tr>${tableHeaders}</tr>`;
 }
 
+//FOLLOWING ON FROM PAGINATION IN writeToDocument FUNCTION
+//create generatePaginationButtons function and pass it next and prev parameters
+function generatePaginationButtons(next, prev) {
+    //we are going to say if there is a value (a URL) for both next and previous, then we will return a template literal that will create buttons
+    if (next && prev) {
+        //we want to assign the onclick function the writeToDocument function and we want it to create the URL, so we use previous and next
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>
+                <button onclick="writeToDocument('${next}')">Next</button>`;
+    } else if (next && !prev) {
+        //else if we have a next URL and no prev URL (at the start of the data pages), all we want is a next button
+        return `<button onclick="writeToDocument('${next}')">Next</button>`;
+    } else if (!next && prev) {
+        //else if we have no next URL and only a prev URL (at the end of the data pages), all we want is a previous button
+        return `<button onclick="writeToDocument('${prev}')">Previous</button>`;
+    }
+}
 
-function writeToDocument(type) {
+function writeToDocument(url) {
     //create a new empty array which will house each row of data for us
     var tableRows = [];
     var el = document.getElementById("data");
     el.innerHTML = "";
 
-    getData(type, function(data) {
+    getData(url, function(data) {
+        //FINAL STEP - PAGINATION
+        //create a pagination varialbe
+        var pagination;
+        //we are going to put an if statement in here to say that if data.next or data.previous exists, we will generate some pagination
+        if (data.next || data.previous) {
+            //create a function called generatePaginationButtons and pass in values of data.next and data.previous - assign it to the pagination variable
+            pagination = generatePaginationButtons(data.next, data.previous)
+        }
+        
         data = data.results;
         //we now call our getTableHeaders function and store the data in our tableHeaders var and pass through the first object in our array
         var tableHeaders = getTableHeaders(data[0]);
@@ -58,6 +86,7 @@ function writeToDocument(type) {
             tableRows.push(`<tr>${dataRow}</tr>`);
         });
         
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+        //once the pagination is done, we need to add the new pagination var to the end of the table
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`;
     });
 }
